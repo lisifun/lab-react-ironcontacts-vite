398b186917826b5c69df6427f571807695e3a45f
lisifun <lisandrafj99@gmail.com>
2023-12-28 16:59:13 -0500

    adding iteration 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,45 @@
+import { useState } from "react";
 import "./App.css";
+import contacts from "./contacts.json";
 
+console.log("line 5 => ", contacts);
+console.log("line 6 => ", contacts.slice(0, 5));
 function App() {
+  const [firstContacts, setFirstContacts] = useState(contacts.slice(0, 5));
+
+  console.log("line 10 => ", firstContacts);
   return (
     <div className="App">
       <h1>LAB | React IronContacts</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+        </thead>
+        <tbody>
+          {firstContacts.map((contact) => {
+            return (
+              <tr>
+                <td>
+                  <img
+                    src={contact.pictureUrl}
+                    style={{ width: "100px", height: "150px" }}
+                  />
+                </td>
+                <td>
+                  <strong>{contact.name}</strong>
+                </td>
+                <td>
+                  <strong>{contact.popularity.toFixed(2)}</strong>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
